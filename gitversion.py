51af0a3eb8d8d6ef02a63aa4d4c094ca0747009a
dai Li <daiybh@gmail.com>
2023-08-09 15:27:44 +0800

    fix the CWD

--- gitversion.py
+++ gitversion.py
@@ -1,8 +1,10 @@
 import subprocess
-
+import os
 def get_git_version():
     try:
-        return subprocess.check_output(['git', 'rev-parse',  'HEAD']).decode('ascii').strip()
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        print(cwd)
+        return subprocess.check_output(['git', 'rev-parse',  'HEAD'],cwd=cwd).decode('ascii').strip()
     except Exception as e:
         print(e)
         return 'unknown'
